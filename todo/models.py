4acf8c0f301283817bef313545832200efe64753
Mahmud Al Hasan <rauaha@gmail.com>
2019-01-15 17:25:20 +0600

    Added database models and written tests

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    """Todo list database model"""
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        """A string representation of the model"""
+        return self.title
+
+
--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from .models import Todo
 # Create your tests here.
+
+
+class TodoModelTest(TestCase):
+    """Test Class"""
+
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='First Todo')
+        Todo.objects.create(description='a description here')
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEquals(expected_object_name, 'First Todo')
+
+    def test_description_content(self):
+        todo = Todo.objects.get(id=2)
+        expected_object_name = f'{todo.description}'
+        self.assertEquals(expected_object_name, 'a description here')
+
+
